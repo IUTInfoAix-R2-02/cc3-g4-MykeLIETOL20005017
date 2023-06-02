5899e5b322688dc45853a3e4b782d14d313b8525
LIETO Myke <lietomyke@gmail.com>
2023-06-02 15:17:50 +0200

    Partie 2 : Essaie de codage d'un event sur les TextField pour ajouter les points sur les lignes

--- src/main/java/fr/amu/iut/cc3/ToileController.java
+++ src/main/java/fr/amu/iut/cc3/ToileController.java
@@ -10,6 +10,7 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Point2D;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
@@ -17,6 +18,8 @@ import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
 
 import java.net.URL;
@@ -32,6 +35,9 @@ public class ToileController implements Initializable {
     private static int noteMaximale = 20;
 
 
+    @FXML
+    private Pane toile;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
     }
@@ -44,6 +50,32 @@ public class ToileController implements Initializable {
     int getYRadarChart(double value, int axe ){
         return (int) (rayonCercleExterieur - Math.sin(Math.toRadians(angleDepart - (axe-1)  * angleEnDegre)) * rayonCercleExterieur
                 *  (value / noteMaximale));
+    }
+    @FXML
+    private void action() {
+        //récupérer la note saisie
+        double note = Double.parseDouble(sourceOfEvent.getText());
+        // identifier la compétence associée au champs de saisie
+        String comptAssocie = sourceOfEvent.getId();
+        int axe = 0;
+        switch (comptAssocie){
+            case "competence1TextField":
+                axe = 1; break;
+            case "competence2TextField":
+                axe = 2; break;
+            case "competence3TextField":
+                axe = 3; break;
+            case "competence4TextField":
+                axe = 4; break;
+            case "competence5TextField":
+                axe = 5; break;
+            case "competence6TextField":
+                axe = 6; break;
+        }
+        // dessiner le point (un cercle JavaFX) sur l'axe correspondant dans le diagramme radar.
+        Circle point = new Circle(10, 10, 10, Color.BLACK);
+        point.setFill(Color.BLACK);
+        toile.getChildren().add(point);
     }
 
 }
